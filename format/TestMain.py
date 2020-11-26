f2a1fd2a4acdea1446e1d855b36df9e33d0e5a58
Kylin <38106679+kylin5207@users.noreply.github.com>
2020-11-26 11:04:42 +0800

    Test Template
    测试模版

---
+++ format/TestMain.py
@@ -0,0 +1,17 @@
+# -- coding = 'utf-8' -- 
+# Author Kylin
+# Python Version 3.7.3
+# OS macOS
+"""
+编写代码模版
+"""
+import numpy as np
+
+
+def main():
+    array = np.arange(0,5)
+    print(array)
+
+
+if __name__ == "__main__":
+    main()
