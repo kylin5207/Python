a44371e4fca4ea67fb5b8d3e81e186525966d35e
Kylin <38106679+kylin5207@users.noreply.github.com>
2022-07-27 10:18:08 +0800

    perf: if for list

--- sugars/if_assignment.py
+++ sugars/if_assignment.py
@@ -1,7 +1,14 @@
 """
-带有 if 选择的条件赋值，其形式为 value = a if condition
+关于if的语法糖
 """
 
+# 带有 if 选择的条件赋值，其形式为 value = a if condition
 voice = "miao"
 value = "cat" if "miao".__eq__(voice) else "dog"
 print(value)
+
+
+# 截断列表中超过5的元素，即超过5的用5代替，小于5的保留原来的值
+L = [1, 2, 3, 4, 5, 6, 7]
+L = [i if i <= 5 else 5 for i in L]
+print(L)
