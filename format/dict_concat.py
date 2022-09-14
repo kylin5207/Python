a15250191b3894ec6e3e2836d425fa600abcf5ec
Kylin <38106679+kylin5207@users.noreply.github.com>
2022-09-14 09:41:21 +0800

    feat: dict concat

---
+++ format/dict_concat.py
@@ -0,0 +1,15 @@
+"""
+字典合并
+"""
+user_data = {"ID": "3312", "name": "Kylin", "degree": "junior"}
+student_data = {"ID": "3312", "score": 78, "degree": "senior"}
+
+# 合并方式一，右边的字段会覆盖左边的同名键
+concat_dict1 = {**user_data, **student_data}
+print(concat_dict1)
+
+# 合并方式2，python v3.9支持
+# 否则会报错，TypeError: unsupported operand type(s) for |: 'dict' and 'dict'
+print(user_data | student_data)
+user_data |= student_data
+print(user_data)
